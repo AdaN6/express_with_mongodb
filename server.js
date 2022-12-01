910372992ca3e2664640931824005be9de3237fb
AdaN6 <a_disney_da@hotmail.com>
2022-12-01 21:08:11 +0100

    created env file

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require ("express")
 const app = express();
+const mongoose = require('mongoose');
+
+
+
 const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Server conencted to port ${port}`))
